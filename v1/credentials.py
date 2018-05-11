8e9dad24a0289ba6201d3eb28c6f97049d26a063
Alexandru Mihaescu <39183560+r0mac09@users.noreply.github.com>
2018-05-11 16:51:46 +0300

    Hah

---
+++ v1/credentials.py
@@ -0,0 +1,76 @@
+import hashlib
+
+def check_email_exists(mail):
+    f = open('C:\\Users\\Mihaescu\\Desktop\\cn-project\\v1\\db.txt', "r")
+    for line in f:
+        user = line.split()
+        if mail == user[1]:
+            f.close()
+            return True
+
+    f.close()
+    return False
+
+def check_user_exists(usr): 
+    f = open('C:\\Users\\Mihaescu\\Desktop\\cn-project\\v1\\db.txt', "r")
+
+    for line in f:
+        user = line.split()
+        if usr == user[0]:
+            f.close()
+            return True
+
+    f.close()    
+    return False
+
+
+
+def check_password_exists(psd_hash):
+    f = open('C:\\Users\\Mihaescu\\Desktop\\cn-project\\v1\\db.txt', "r")
+
+    for line in f:
+        user = line.split()
+        if psd_hash == user[2].decode("hex"):
+            f.close()
+            return True
+    
+    f.close()
+    return False
+
+class Login():
+    def __init__(self, usr, psd):
+        self.u = usr
+        self.p = psd
+        self.h = hashlib.sha256(psd.encode()).hexdigest()
+
+    def check_credentials(self):
+        if check_user_exists(self.u):
+            print("Login")
+        else:
+            print("No way you'll be")
+        if check_password_exists(self.h):
+            print("Successful!")
+        else:
+            print("Failed")
+
+    def authenticate_user(self):
+        f = open('C:\\Users\\Mihaescu\\Desktop\\cn-project\\v1\\db.txt', "r")
+        for line in f:
+            user = line.split()
+            if self.u == user[0] and self.h == user[2].decode('hex'):
+                return True
+
+        return False
+
+
+class SignUp():
+    def __init__(self, user, email, password):
+        self.u = user
+        self.e = email
+        self.p = password
+        self.h = hashlib.sha256(password.encode()).hexdigest()
+    
+    def add_db(self):
+        f = open('C:\\Users\\Mihaescu\\Desktop\\cn-project\\v1\\db.txt', 'a')
+        f.write(self.u + ' ' + self.e + ' ' + self.h)
+        f.close()
--- v1/gui.py
+++ v1/gui.py
@@ -1,16 +1,91 @@
 import tkinter
-import login
+import credentials
 import session
 	
 from tkinter import *
-from login import *
+from credentials import *
 from session import *
+
+class SessionWindow():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Session v0.1")
+        self.window.geometry('350x200')
+
+        self.window.mainloop()
+
+
+class SignUpWindow():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Sign Up v0.1")
+        self.window.geometry('350x350')
+
+        signup_label = Label(self.window, text="Sign Up", font=("Helvetica", 14))
+        signup_label.grid(column=0, row=0)
+
+        usr_label = Label(self.window, text="User:", font=("Helvetica", 12))
+        usr_label.grid(column=0, row=1)
+
+        self.usr = Entry(self.window, width=24)
+        self.usr.grid(column=1, row=1)
+
+        eml_label = Label(self.window, text="Email:", font=("Helvetica", 12))
+        eml_label.grid(column=0, row=2)
+
+        self.eml = Entry(self.window, width=24)
+        self.eml.grid(column=1, row=2)
+
+        psd_label = Label(self.window, text="Password:", font=("Helvetica", 12))
+        psd_label.grid(column=0, row=3)
+
+        self.psd = Entry(self.window, width=24)
+        self.psd.grid(column=1, row=3)
+
+        repsd_label = Label(self.window, text="Re-Password:", font=("Helvetica", 12))
+        repsd_label.grid(column=0, row=4)
+
+        self.repsd = Entry(self.window, width=24)
+        self.repsd.grid(column=1, row=4)
+
+        signup_button = Button(self.window, text="Sign Up", bg="light blue", fg="white", command=self.signup_action)
+        signup_button.grid(column=1, row=6)
+
+        cancel_button = Button(self.window, text="Cancel", bg="red", fg="white", command=self.cancel_action)
+        cancel_button.grid(column=2, row=6)
+
+        self.window.mainloop()
+
+    def signup_action(self):
+        user = self.usr.get()
+        email = self.eml.get()
+        password = self.psd.get()
+        repassword = self.repsd.get()
+
+        if check_user_exists(user):
+            usr_exist_label = Label(self.window, text="user already taken", font=("Helvetica", 12))
+            usr_exist_label.grid(column=3, row=1)
+        elif check_email_exists(email):
+            eml_exist_label = Label(self.window, text="email already used", font=("Helvetica", 12))
+            eml_exist_label.grid(column=3, row=2)
+        elif not password == repassword:
+            psd_no_match_label = Label(self.window, text="passwords must match", font=("Helvetica", 12))
+            psd_no_match_label.grid(column=3, row=4)
+        elif not user == None and not email == None and not password == None:
+            s = SignUp(user, email, password)
+            s.add_db()
+        
+        
+
+    def cancel_action(self):
+        self.window.destroy()
+
 
 class LoginWindow():
     def __init__(self):
         self.window = Tk()
         self.window.title("CNizator v0.1")
-        self.window.geometry('350x200')
+        self.window.geometry('270x200')
 
         login_label = Label(self.window, text="Login", font=("Helvetica", 14))
         login_label.grid(column=0, row=0)
@@ -36,7 +111,7 @@ class LoginWindow():
         ots_button = Button(self.window, text ="One Time Session", bg="orange", fg="white")
         ots_button.grid(column=0, row=4)
 
-        signup_button = Button(self.window, text ="Sign Up", bg="orange", fg="white")
+        signup_button = Button(self.window, text ="Sign Up", bg="orange", fg="white", command=self.signup_action)
         signup_button.grid(column=1, row=4)
 
         self.window.mainloop()
@@ -46,12 +121,25 @@ class LoginWindow():
         password = self.psd.get()
         self.test_label.configure(text=user + password)
 
+        if user == "":
+            nousr_label = Label(self.window, text='no user', font=("Helvetica", 12))
+            nousr_label.grid(column=2, row=1)
+
+        if password == "":
+            nopsd_label = Label(self.window, text='no password', font=("Helvetica", 12))
+            nopsd_label.grid(column=2, row=2)
+
+
         l = Login(user, password)
         if l.authenticate_user():
-            session_window = Session(user)
+            session_window = SessionWindow()
             self.usr = Entry(self.window, width=24, state='disabled')
             self.psd = Entry(self.window, width=24, state='disabled')
 
-
+    def signup_action(self):
+        s = SignUpWindow()
 
 login_window = LoginWindow()
+
+
+
